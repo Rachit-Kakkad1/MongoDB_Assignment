caa87460347fd9419a465c1aee527c100fbd3e0d
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-06-02 21:56:35 +0530

    feat: add get all notes endpoint (GET /api/notes)

--- Notes_Management_A2/src/routes/note.routes.js
+++ Notes_Management_A2/src/routes/note.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, createBulkNotes } = require("../controllers/note.controller");
+const { createNote, createBulkNotes, getAllNotes } = require("../controllers/note.controller");
 
 router.post("/", createNote);
 router.post("/bulk", createBulkNotes);
+router.get("/", getAllNotes);
 
 module.exports = router;
