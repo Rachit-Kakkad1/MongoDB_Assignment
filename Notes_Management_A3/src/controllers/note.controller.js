3f2126dbea288b46559ef9e4551cb245952cb5d2
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-06-02 23:21:33 +0530

    feat: add get all notes endpoint

--- Notes_Management_A3/src/controllers/note.controller.js
+++ Notes_Management_A3/src/controllers/note.controller.js
@@ -63,7 +63,26 @@ const createBulkNotes = async (req, res) => {
   }
 };
 
+const getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find({});
+
+    res.status(200).json({
+      success: true,
+      message: "Notes retrieved successfully",
+      data: notes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
 module.exports = {
   createNote,
-  createBulkNotes
+  createBulkNotes,
+  getAllNotes
 };
